fcebf90a393e3a72a4e736b4d8d6bee5e37bceaf
HAICU Dominik Thalmeier <dominik.thalmeier@clara.scidom.de>
2021-08-19 14:39:37 +0200

    added setup and raw structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='ForestGuidedClustering',
+    version='0.1.dev0',
+    packages=setuptools.find_packages(),
+    install_requires=['pandas','numpy','matplotlib','sklearn','scikit-learn-extra'],
+    long_description=open('README.md').read(),
+)
