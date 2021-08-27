04f2efc72499284807e4cc306d2ea332de4c6afe
lisa-sousa <lisa.barros@helmholtz-muenchen.de>
2021-08-27 22:15:11 +0200

    created toy dataset for regression and classification

--- test/test_proximity.R
+++
@@ -1,8 +0,0 @@
-
-library(datasets)
-library(randomForest)
-data(iris)
-
-iris.rf <- randomForest(Species ~ ., data=iris, importance=TRUE, proximity=TRUE, ntree=500, mtry=2)
-iris.rf$err.rate
-iris.rf$proximity
