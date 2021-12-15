4abac6a3207d18c2854bf672342df54c29206e71
DoTha <dominikthalmeier@googlemail.com>
2021-12-15 17:25:53 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,5 @@ setup(
     version='0.1.dev0',
     packages=setuptools.find_packages(),
     install_requires=['pandas','numpy','matplotlib','seaborn','sklearn','scikit-learn-extra','scipy','tqdm', 'statsmodels'],
-    docs_extras = [
-        'Sphinx >= 3.0.0',  # Force RTD to use >= 3.0.0
-        'docutils',
-        'pylons-sphinx-themes >= 1.0.8',  # Ethical Ads
-        'pylons_sphinx_latesturl',
-        'repoze.sphinx.autointerface',
-        'sphinx-copybutton',
-        'sphinxcontrib-autoprogram',
-    ],
     long_description=open('README.md').read(),
 )
