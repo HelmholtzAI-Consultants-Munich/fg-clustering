0d85f746b9e339b7ca2b832af93eefa2892a27b7
DoTha <dominikthalmeier@googlemail.com>
2021-12-15 17:38:15 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,8 @@ setup(
     version='0.1.dev0',
     packages=setuptools.find_packages(),
     install_requires=['pandas','numpy','matplotlib','seaborn','sklearn','scikit-learn-extra','scipy','tqdm', 'statsmodels'],
+    docs_extras = [
+        'Sphinx = 4.2.0',  # Force RTD to use >= 3.0.0
+    ]
     long_description=open('README.md').read(),
 )
