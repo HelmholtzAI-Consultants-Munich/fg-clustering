d3c207b3c06457317cb5fc2e29e11041caa93299
Lisa B. A. Sousa <44869855+lisa-sousa@users.noreply.github.com>
2021-12-15 18:27:38 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version='0.1.dev0',
     packages=setuptools.find_packages(),
     install_requires=['pandas','numpy','matplotlib','seaborn','sklearn','scikit-learn-extra','scipy','tqdm', 'statsmodels','Sphinx==4.2.0'],
-    long_description=open('README.md').read(),
+    long_description=open('README.rst').read(),
 )
