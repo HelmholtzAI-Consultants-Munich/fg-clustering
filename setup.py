c420acacc4660cae0ec982fd5250e64e5c7116bf
DoTha <dominikthalmeier@googlemail.com>
2021-12-15 17:55:56 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name='ForestGuidedClustering',
     version='0.1.dev0',
     packages=setuptools.find_packages(),
-    install_requires=['pandas','numpy','matplotlib','seaborn','sklearn','scikit-learn-extra','scipy','tqdm', 'statsmodels','Sphinx = 4.2.0'],
+    install_requires=['pandas','numpy','matplotlib','seaborn','sklearn','scikit-learn-extra','scipy','tqdm', 'statsmodels','Sphinx==4.2.0'],
     long_description=open('README.md').read(),
 )
