26d76644cff5f9102aea2a9a4ef38c81384e0729
DoTha <dominikthalmeier@googlemail.com>
2021-12-15 17:53:40 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,9 @@
 from distutils.core import setup
 
-docs_extras = [
-        'Sphinx = 4.2.0',  # Force RTD to use >= 3.0.0
-    ]
-
 setup(
     name='ForestGuidedClustering',
     version='0.1.dev0',
     packages=setuptools.find_packages(),
-    install_requires=['pandas','numpy','matplotlib','seaborn','sklearn','scikit-learn-extra','scipy','tqdm', 'statsmodels'],
-    extras_require={'docs': docs_extras},
+    install_requires=['pandas','numpy','matplotlib','seaborn','sklearn','scikit-learn-extra','scipy','tqdm', 'statsmodels','Sphinx = 4.2.0'],
     long_description=open('README.md').read(),
 )
