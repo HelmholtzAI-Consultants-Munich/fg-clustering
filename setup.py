d1c3f84e3be10581c098387636875785ad2ad56e
DoTha <dominikthalmeier@googlemail.com>
2022-01-13 16:26:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='ForestGuidedClustering',
-    version='0.1.dev0',
+    version='0.1.0',
     packages=setuptools.find_packages(),
     install_requires=['pandas','numpy','matplotlib','seaborn','sklearn','scikit-learn-extra','scipy','tqdm', 'statsmodels','Sphinx==4.2.0'],
     long_description=open('README.rst').read(),
