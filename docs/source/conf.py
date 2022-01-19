ddea36e4a66b11cf3cd7db8bd5e3499a73aa2b98
DoTha <dominikthalmeier@googlemail.com>
2022-01-19 15:42:39 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,8 +29,9 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'nbsphinx'
+extensions = ['sphinx.ext.autodoc', 'nbsphinx', sphinxcontrib.bibtex
 ]
+bibtex_bibfiles = ['refs.bib']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
