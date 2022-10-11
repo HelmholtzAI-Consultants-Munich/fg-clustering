6ac22256fbd7f17d08c7e56a26ba171b03ec2d8c
hpelin <helena.pelin@gmail.com>
2022-10-11 12:10:14 +0200

    added n_jobs argument

--- test/test_forest_guided_clustering.py
+++ test/test_forest_guided_clustering.py
@@ -23,10 +23,10 @@ def test_forest_guided_clustering():
     
     # initialize and run fgclustering object
     fgc = FgClustering(model=rf, data=data_breast_cancer, target_column='target')
-    fgc.run(max_K = 4, bootstraps_JI = 30, max_iter_clustering = 100, discart_value_JI = 0.6)
+    fgc.run(max_K = 6, bootstraps_JI = 30, max_iter_clustering = 500, discart_value_JI = 0.6, n_jobs = 2)
 
     # obtain optimal number of clusters and vector that contains the cluster label of each data point
     result = fgc.k
-    
+
     assert result == expected_output
 
