8d2dbea13459a395c34c1c334ca917f5d5c78861
hpelin <helena.pelin@gmail.com>
2022-10-12 11:47:52 +0200

    parallelized jaccard loop (code not cleaned yet)

--- fgclustering/optimizer.py
+++ fgclustering/optimizer.py
@@ -7,6 +7,7 @@ from tqdm import tqdm
 
 from sklearn_extra.cluster import KMedoids
 from joblib import Parallel, delayed
+import collections, functools, operator
 
 import fgclustering.statistics as statistics
 
@@ -48,7 +49,7 @@ def _compute_jaccard_matrix(clusters, indices_bootstrap_clusters, indices_origin
     return jaccard_matrix
 
 
-def _bootstrap_matrix(M):
+def _bootstrap_matrix(M, seed_int):
     '''Create a bootstrap from the original matrix.
 
     :param M: Original matrix.
@@ -56,7 +57,9 @@ def _bootstrap_matrix(M):
     :return: M_bootstrapped: ootstrapped matrix; 
         mapping_bootstrapped_indices_to_original_indices: mapping from bootstrapped to original indices.
     :rtype: pandas.DataFrame, dict
-    '''    
+    '''
+
+    np.random.seed(seed_int)
     lm = len(M)
     bootstrapped_samples = np.random.choice(np.arange(lm), lm)
     bootstrapped_samples = np.sort(bootstrapped_samples)
@@ -93,7 +96,31 @@ def _translate_cluster_labels_to_dictionary_of_index_sets_per_cluster(labels, ma
     return indices_clusters
 
 
-def _compute_stability_indices(distance_matrix, labels, cluster_method, bootstraps, random_state):
+def _optimizeJaccardloop(bootstrap_index, distance_matrix, cluster_method, clusters, indices_original_clusters):
+    bootstrapped_distance_matrix, mapping_bootstrapped_indices_to_original_indices = _bootstrap_matrix(distance_matrix, bootstrap_index)
+    bootstrapped_labels = cluster_method(bootstrapped_distance_matrix)
+
+    # now compute the indices for the different clusters
+    indices_bootstrap_clusters = _translate_cluster_labels_to_dictionary_of_index_sets_per_cluster(bootstrapped_labels,
+                                                                                                   mapping=mapping_bootstrapped_indices_to_original_indices)
+    jaccard_matrix = _compute_jaccard_matrix(clusters, indices_bootstrap_clusters, indices_original_clusters)
+
+    index_per_cluster = {cluster: 0 for cluster in clusters}
+    # compute optimal jaccard index for each cluster -> choose maximum possible jaccard index first
+    for cluster_round in range(len(jaccard_matrix)):
+        best_index = jaccard_matrix.max(axis=1).max()
+        original_cluster_number = jaccard_matrix.max(axis=1).argmax()
+        bootstrapped_cluster_number = jaccard_matrix[original_cluster_number].argmax()
+        jaccard_matrix[original_cluster_number] = -np.inf
+        jaccard_matrix[:, bootstrapped_cluster_number] = -np.inf
+
+        original_cluster = clusters[original_cluster_number]
+        index_per_cluster[original_cluster] += best_index
+    #print(f'From parallel loop {index_per_cluster}')
+
+    return index_per_cluster
+
+def _compute_stability_indices(distance_matrix, labels, cluster_method, bootstraps, random_state, n_jobs):
     '''Compute stability of each cluster via Jaccard Index of bootstraped vs original clustering.
 
     :param distance_matrix: Proximity matrix of Random Forest model.
@@ -110,7 +137,7 @@ def _compute_stability_indices(distance_matrix, labels, cluster_method, bootstra
     :rtype: dict
     '''
 
-    np.random.seed = random_state
+    np.random.seed(random_state)
 
     clusters = np.unique(labels)
     # number_datapoints = len(labels) # This is not used at all here - TODO: Delete
@@ -118,33 +145,51 @@ def _compute_stability_indices(distance_matrix, labels, cluster_method, bootstra
     
     indices_original_clusters = _translate_cluster_labels_to_dictionary_of_index_sets_per_cluster(labels)
     index_per_cluster = {cluster: 0 for cluster in clusters}
-    
-    for i in range(bootstraps):
-        bootstrapped_distance_matrix, mapping_bootstrapped_indices_to_original_indices = _bootstrap_matrix(distance_matrix)
-        bootstrapped_labels = cluster_method(bootstrapped_distance_matrix)
-        
-        # now compute the indices for the different clusters
-        indices_bootstrap_clusters = _translate_cluster_labels_to_dictionary_of_index_sets_per_cluster(bootstrapped_labels, mapping = mapping_bootstrapped_indices_to_original_indices)
-        jaccard_matrix = _compute_jaccard_matrix(clusters, indices_bootstrap_clusters, indices_original_clusters)
-        
-        # compute optimal jaccard index for each cluster -> choose maximum possible jaccard index first
-        for cluster_round in range(len(jaccard_matrix)):
-            best_index = jaccard_matrix.max(axis=1).max()       
-            original_cluster_number = jaccard_matrix.max(axis=1).argmax()
-            bootstrapped_cluster_number = jaccard_matrix[original_cluster_number].argmax()
-            jaccard_matrix[original_cluster_number] = -np.inf
-            jaccard_matrix[:,bootstrapped_cluster_number] = -np.inf
 
-            original_cluster = clusters[original_cluster_number]
-            index_per_cluster[original_cluster] += best_index
-                                    
-    # normalize
+    index_per_cluster = Parallel(n_jobs=n_jobs)(
+        delayed(_optimizeJaccardloop)(bootstrap_index, distance_matrix, cluster_method, clusters,
+                                      indices_original_clusters) for bootstrap_index in range(bootstraps))
+    print(f'Nr bootstraps {bootstraps}')
+    print(f'NEW --> {index_per_cluster}')
+    # Sum values of the same keys across dictionaries:
+    index_per_cluster = dict(functools.reduce(operator.add,
+                                   map(collections.Counter, index_per_cluster)))
+    print(f'NEW final result: {index_per_cluster}')
+    # Normalise:
     index_per_cluster = {cluster: index_per_cluster[cluster]/bootstraps for cluster in clusters}
-        
+    print(f'NEW normalizes final result: {index_per_cluster}')
+
+    # for i in range(bootstraps):
+    #     bootstrapped_distance_matrix, mapping_bootstrapped_indices_to_original_indices = _bootstrap_matrix(distance_matrix, seed_int = int(i))
+    #     bootstrapped_labels = cluster_method(bootstrapped_distance_matrix)
+    #
+    #     # now compute the indices for the different clusters
+    #     indices_bootstrap_clusters = _translate_cluster_labels_to_dictionary_of_index_sets_per_cluster(bootstrapped_labels, mapping = mapping_bootstrapped_indices_to_original_indices)
+    #     jaccard_matrix = _compute_jaccard_matrix(clusters, indices_bootstrap_clusters, indices_original_clusters)
+    #
+    #     # compute optimal jaccard index for each cluster -> choose maximum possible jaccard index first
+    #     for cluster_round in range(len(jaccard_matrix)):
+    #         best_index = jaccard_matrix.max(axis=1).max()
+    #         original_cluster_number = jaccard_matrix.max(axis=1).argmax()
+    #         bootstrapped_cluster_number = jaccard_matrix[original_cluster_number].argmax()
+    #         jaccard_matrix[original_cluster_number] = -np.inf
+    #         jaccard_matrix[:,bootstrapped_cluster_number] = -np.inf
+    #
+    #         original_cluster = clusters[original_cluster_number]
+    #         index_per_cluster[original_cluster] += best_index
+    #         #if i < 3 :
+    #             #print(f'original cluster {original_cluster}')
+    #             #print(f'best index {best_index}')
+    #             #print(f'From the loop {index_per_cluster}')
+    #     #print(f'Index per cluster from bootstrap sample {i}: {index_per_cluster}')
+    # print(f'Index per cluster all bootstraps: {index_per_cluster}')
+    # # normalize
+    # index_per_cluster = {cluster: index_per_cluster[cluster]/bootstraps for cluster in clusters}
+    # print(f'Index per cluster normalized: {index_per_cluster}')
     return index_per_cluster
     
 
-def _optimizeKloop(k, distance_matrix, y, random_state, max_iter_clustering, bootstraps, discart_value, method):
+def _optimizeKloop(k, distance_matrix, y, random_state, max_iter_clustering, bootstraps, discart_value, method, n_jobs):
     '''Compute the optimal number of clusters for k-medoids clustering - loop that is being paralelized in the optimizeK function
     :param k: number of clusters for the K-medoids call
     :type k: int
@@ -160,6 +205,8 @@ def _optimizeKloop(k, distance_matrix, y, random_state, max_iter_clustering, boo
     :type discart_value: float
     :param method: Model type of Random Forest model: classifier or regression.
     :type method: str
+    :param n_jobs: number of jobs to run in parallel when computing jaccard over bootstrap samples. It is the same as for the n_jobs of optimizing Kloop.
+    :type n_jobs: int, optional
     '''
     # compute clusters
     print(f'Checking number of clusters k={k}')
@@ -168,7 +215,7 @@ def _optimizeKloop(k, distance_matrix, y, random_state, max_iter_clustering, boo
     labels = cluster_method(distance_matrix)
 
     # compute jaccard indices
-    index_per_cluster = _compute_stability_indices(distance_matrix, labels, cluster_method, bootstraps, random_state)
+    index_per_cluster = _compute_stability_indices(distance_matrix, labels, cluster_method, bootstraps, random_state, n_jobs)
     min_index = min([index_per_cluster[cluster] for cluster in index_per_cluster.keys()])
 
     # only continue if jaccard indices are all larger 0.6 (thus all clusters are stable)
@@ -218,10 +265,12 @@ def optimizeK(distance_matrix, y, max_K, bootstraps, max_iter_clustering, discar
     assert matrix_shape[ 0 ] == matrix_shape[ 1 ], "error distance matrix is not square"
 
     results = Parallel(n_jobs=n_jobs)(
-        delayed(_optimizeKloop)(k, distance_matrix, y, random_state, max_iter_clustering, bootstraps, discart_value, method) for k
+        delayed(_optimizeKloop)(k, distance_matrix, y, random_state, max_iter_clustering, bootstraps, discart_value, method, n_jobs) for k
         in range(2, max_K))
+    print(f'results from K optimize loop {results}')
     # Flat to dictionary:
     results = dict(sum(map(list, map(dict.items, results)), []))
+    print(f'results flattened: {results}')
     optimal_k, score = min(results.items(), key=lambda k: k[1])  # optimal k is the one with minimum impurity/within cluster variation score
 
     return optimal_k
