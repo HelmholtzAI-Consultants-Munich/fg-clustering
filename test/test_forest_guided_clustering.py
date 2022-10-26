49b59ff9bbd4fe021e412f6d541242f47114cb14
Lisa B. A. Sousa <44869855+lisa-sousa@users.noreply.github.com>
2022-10-26 17:08:47 +0200

    formatting

--- test/test_forest_guided_clustering.py
+++ test/test_forest_guided_clustering.py
@@ -19,14 +19,18 @@ def test_forest_guided_clustering():
     data_breast_cancer = pd.read_csv('./data/data_breast_cancer.csv')
     rf = joblib.load(open('./data/random_forest_breat_cancer.joblib', 'rb'))
     
-    expected_output = 2
+    max_K = 6
+    bootstraps_JI = 30
+    max_iter_clustering = 100
+    discart_value_JI = 0.6
+    n_jobs = 3
     
     # initialize and run fgclustering object
     fgc = FgClustering(model=rf, data=data_breast_cancer, target_column='target')
-    fgc.run(max_K = 6, bootstraps_JI = 30, max_iter_clustering = 500, discart_value_JI = 0.6, n_jobs = 3)
+    fgc.run(max_K, bootstraps_JI, max_iter_clustering, discart_value_JI, n_job)
 
     # obtain optimal number of clusters and vector that contains the cluster label of each data point
     result = fgc.k
 
-    assert result == expected_output
+    assert result == 2
 
