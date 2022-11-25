c17c449b0d092ad01dec958bc5605152540e829e
hpelin <helena.pelin@gmail.com>
2022-11-25 17:40:19 +0100

    missing packages and python version

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     author='Lisa Barros de Andrade e Sousa, Dominik Thalmeier, Helena Pelin, Marie Piraud',
     author_email='lisa.barros.andrade.sousa@gmail.com, dominikthalmeier@googlemail.com, helena.pelin@helmholtz-muenchen.de',
     packages=setuptools.find_packages(),
-    install_requires=['pandas','numpy','matplotlib','seaborn>=0.12','sklearn','kmedoids','scipy','tqdm', 'statsmodels>=0.13.5','Sphinx>=4.2.0', 'numexpr>=2.8.4'],
+    install_requires=['pandas','numpy','matplotlib','seaborn>=0.12','scikit-learn','kmedoids','scipy','tqdm', 'statsmodels>=0.13.5','Sphinx>=4.2.0', 'numexpr>=2.8.4', 'numba'],
+    python_requires='<3.11.0',
     long_description=open('README.rst').read(),
 )
