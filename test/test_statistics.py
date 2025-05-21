9c3ec7b76b4a76180cd2c84f45fcc1f5d6956dc4
gregor.miller <gregor.miller@helmholtz-munich.de>
2025-05-21 11:22:48 +0200

    further integrated the new feature importance calculation in dependencies including tests

--- test/test_statistics.py
+++ test/test_statistics.py
@@ -9,8 +9,7 @@ from fgclustering.utils import *
 from fgclustering.statistics import (
     compute_balanced_average_impurity,
     compute_total_within_cluster_variation,
-    calculate_global_feature_importance,
-    calculate_local_feature_importance,
+    calculate_feature_importance,
 )
 
 
@@ -57,7 +56,7 @@ def test_calculate_global_feature_importance():
     y = pd.Series([0, 0, 0, 0, 0, 0])
     cluster_labels = np.array([0, 0, 0, 1, 1, 1])
 
-    X_ranked, p_value_of_features = calculate_global_feature_importance(X, y, cluster_labels, model_type)
+    feature_importance_local, feature_importance_global, X_ranked = calculate_feature_importance(X, y, cluster_labels, model_type=model_type)
 
     X_ranked.drop("cluster", axis=1, inplace=True)
     assert list(X_ranked.columns) == [
@@ -72,8 +71,7 @@ def test_calculate_global_feature_importance():
 def test_calculate_local_feature_importance():
     # test if clusterwise importance is high for feature 2 and 4 and low for feature 1 and 3
     # parameters
-    thr_pvalue = 1
-    bootstraps = 100
+    thr_distance = 0
     model_type = "classifier"
 
     # test data
@@ -88,13 +86,54 @@ def test_calculate_local_feature_importance():
     y = pd.Series([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
     cluster_labels = np.array([0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2])
 
-    X_ranked, p_value_of_features = calculate_global_feature_importance(X, y, cluster_labels, model_type)
+    feature_importance_local, feature_importance_global, X_ranked = calculate_feature_importance(X, y, cluster_labels, model_type=model_type)
     for column in X.columns:
-        if p_value_of_features.loc["p_value", column] > thr_pvalue:
+        if feature_importance_global.loc[column] > thr_distance:
             X.drop(column, axis=1, inplace=True)
 
-    p_value_of_features_per_cluster = calculate_local_feature_importance(X_ranked, bootstraps)
-    importance = 1 - p_value_of_features_per_cluster
+    importance = feature_importance_local
     result = importance.transpose().median()
+    
+    assert sum(result > 0.1) == 2, "error: wrong number of features with highest feature importance"
 
-    assert sum(result > 0.9) == 2, "error: wrong number of features with highest feature importance"
+
+def test_feature_importance_all_below_threshold():
+    X = pd.DataFrame(
+        {
+            "col_1": [1, 1, 1, 1],
+            "col_2": [0, 0, 0, 0],
+        }
+    )
+    y = pd.Series([0, 0, 1, 1])
+    cluster_labels = np.array([0, 0, 1, 1])
+    model_type = "classifier"
+
+    feature_importance_local, feature_importance_global, X_ranked = calculate_feature_importance(X, y, cluster_labels, model_type=model_type)
+
+    # Here we assume a threshold of 0.5, which no feature should exceed
+    thr_distance = 0.5
+    retained = feature_importance_global[feature_importance_global > thr_distance]
+    
+    assert retained.empty, "error: no features should pass the threshold"
+
+
+def test_feature_importance_with_different_distance_funcs():
+    # Construct data with clearly distinguishable distributions
+    X = pd.DataFrame({
+        "cont_feature": [0.1, 0.2, 0.2, 0.3, 0.8, 0.9, 0.95, 1.0],  # continuous
+        "cat_feature": [0, 0, 0, 0, 1, 1, 1, 1],                    # categorical
+    })
+    y = pd.Series([0, 0, 0, 0, 1, 1, 1, 1])  # Dummy target
+    cluster_labels = np.array([0, 0, 0, 0, 1, 1, 1, 1])
+
+    fi_local_w, fi_global_w, _ = calculate_feature_importance(
+        X, y, cluster_labels, distance_func="wasserstein", model_type="classifier"
+    )
+
+    fi_local_js, fi_global_js, _ = calculate_feature_importance(
+        X, y, cluster_labels, distance_func="jensen-shannon", model_type="classifier"
+    )
+
+    # They should not be exactly the same
+    assert not fi_global_w.equals(fi_global_js), "error: distance functions return identical results"
+
