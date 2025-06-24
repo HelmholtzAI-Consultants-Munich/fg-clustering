3e986436b26834c5a4a59d4034579814384be57b
lisa-sousa <lisa.barros.andrade.sousa@gmail.com>
2025-06-24 20:44:26 +0200

    show only relevant modules

--- fgclustering/__init__.py
+++ fgclustering/__init__.py
@@ -2,10 +2,6 @@
 Forest-Guided Clustering (FGC) is an explainability method for Random Forest models that addresses one of the key limitations of many standard XAI techniques: the inability to effectively handle correlated features and complex decision patterns.
 """
 
-from .clustering import ClusteringKMedoids, ClusteringClara
-
-from .distance import DistanceRandomForestProximity, DistanceJensenShannon, DistanceWasserstein
-
 from .forest_guided_clustering import (
     forest_guided_clustering,
     forest_guided_feature_importance,
@@ -13,21 +9,25 @@ from .forest_guided_clustering import (
     plot_forest_guided_decision_paths,
 )
 
+from .clustering import ClusteringKMedoids, ClusteringClara
+
+from .distance import DistanceRandomForestProximity, DistanceJensenShannon, DistanceWasserstein
+
 from .optimizer import Optimizer
 
 from .statistics import FeatureImportance
 
 
 __all__ = [
+    "forest_guided_clustering",
+    "forest_guided_feature_importance",
+    "plot_forest_guided_feature_importance",
+    "plot_forest_guided_decision_paths",
     "ClusteringKMedoids",
     "ClusteringClara",
     "DistanceRandomForestProximity",
     "DistanceJensenShannon",
     "DistanceWasserstein",
-    "forest_guided_clustering",
-    "forest_guided_feature_importance",
-    "plot_forest_guided_feature_importance",
-    "plot_forest_guided_decision_paths",
     "Optimizer",
     "FeatureImportance",
 ]
